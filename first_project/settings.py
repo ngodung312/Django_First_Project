c3e43b0d5b1c64b1075e2d75ca258053b0422baf
FIXED-TERM Ngo Le Hanh Dung (MS/EDA15-XC) <gdl1hc@bosch.com>
2022-07-11 11:07:23 +0700

    Update allowed hosts

--- first_project/settings.py
+++ first_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#!wa#oaeqxn$)g81k54w4e&bz=sa9ac_x(fp00!1jr0!^&7n1t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://gdl1hc.pythonanywhere.com/']
+ALLOWED_HOSTS = ['http://gdl1hc.pythonanywhere.com/', 'gdl1hc.pythonanywhere.com']
 
 
 # Application definition
