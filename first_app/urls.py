454b375a078eb8cdda08a6d783d03744b5f3afc4
FIXED-TERM Ngo Le Hanh Dung (MS/EDA15-XC) <gdl1hc@bosch.com>
2022-07-12 15:41:05 +0700

    Version 0.4: Update base template

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from first_app import views 
 
+app_name = 'first_app'
+
 urlpatterns = [
     path(r'', views.main_page, name='main_page'),
     path(r'access_records/', views.access_records, name='access_records'),
