82756b4f486feb6bd23bf96ace47a268f2aee9a5
FIXED-TERM Ngo Le Hanh Dung (MS/EDA15-XC) <gdl1hc@bosch.com>
2022-07-12 14:02:02 +0700

    Fix error verifying email

--- first_app/forms.py
+++ first_app/forms.py
@@ -11,6 +11,14 @@ class UserForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': "Enter email"}),
             'verify_email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': "Enter email again"}),
             'text': forms.Textarea(attrs={'class': 'form-control', 'placeholder': "Enter text"}),
-        }      
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get("email")
+        vmail = cleaned_data.get("verify_email")
+        print(email, vmail)
+        if email != vmail:
+            raise forms.ValidationError("MAKE SURE EMAILS MATCH!")       
 
         
